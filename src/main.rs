afa821585497d9305dd0ded709c47be5abff4b77
Eren <erenyordem2006@gmail.com>
2025-02-26 19:58:27 +0300

    init.

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Falling Sand Simulator".into(),
+                resolution: (1000.0, 1000.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Spawn a 2D camera
+    commands.spawn(Camera2d::default());
+}
